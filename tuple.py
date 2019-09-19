d49d14095495312b76dfa90048255029fe98c3a0
Shishir <sishishir2015@gmail.com>
2019-09-19 17:25:39 +0600

    Tuple

---
+++ tuple.py
@@ -0,0 +1,17 @@
+tuple = (1,2,'Shishir',4.4,'Boss',False)
+
+print(tuple)
+
+#Accessing Tuple: Same As Lists:
+
+print(tuple[0],tuple[-1])
+
+#Iteration:
+
+for tp in tuple:
+    print(tp)
+
+#Unpacking:
+tpl1=(5,7,11)
+x,y,z=tpl1
+print(x,y,z ,sep='|')
