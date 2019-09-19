1b091bcf79f1a2102571b664fe49d4ed526c437b
Shishir <sishishir2015@gmail.com>
2019-09-19 19:45:03 +0600

    Function

---
+++ Function.py
@@ -0,0 +1,36 @@
+#Basic Function Call:
+
+def welcome():
+    x = 'Welcome To Python'
+    print(x)
+
+welcome()
+
+
+#Function Call With Iterartion:
+
+def hello():
+    x="Hello World"
+    for wel in x:
+        print(x)
+
+hello()
+
+
+def hello_2():
+    x1='Shishir'
+    for name in x1:
+        print(x1)
+hello_2()
+
+
+#Parameter Passing:
+
+def new_name(name):
+    name_1 = name
+    name_2 = name
+    print("Welcome,{}".format(name_1.lower()))
+    print("Welcome {}".format(name_2.upper()))
+
+new_name('SHIShIR')
+new_name("The_Presence")
---
+++ test1.py
@@ -0,0 +1,5 @@
+def h1():
+    x2='Shishir'
+    for x3 in x2:
+        print(x2)
+h1()
