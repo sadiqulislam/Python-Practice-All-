01740695e938e6918dc367355b0410584646c70e
Shishir <sishishir2015@gmail.com>
2019-10-06 19:49:26 +0600

    Method Overriding And Call Class From Method

---
+++ MethodOverriding.py
@@ -0,0 +1,24 @@
+#Method Overriding And Call Class From Method:
+
+class Person():
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def details(self):
+        return str(self.name) + " " + str(self.age)
+
+class Address(Person):
+    def __init__(self,name,age):
+        super().__init__(name,age)
+
+    def details(self):
+        return str(self.name) + " " + str(self.age) + " " + "Dhaka"
+
+    def full_details(self):
+        print("Details Of Parent Class Without Address",super().details())
+        print("Details Of Child Class With Address",self.details())
+
+
+person = Address("Shishir",23)
+person.full_details()
