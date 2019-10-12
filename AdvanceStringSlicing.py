5c5757654fdc16501bb747a8f61ad0b69f17584c
Shishir <sishishir2015@gmail.com>
2019-10-12 23:34:17 +0600

    Simple Add Calculator And Advance String Slicing

---
+++ AdvanceStringSlicing.py
@@ -0,0 +1,4 @@
+mystr = "Shishir Is A Good Boy"
+
+print(mystr[0:7:2])  #skipped 1 letter
+print(mystr[0:7:3])  #skipped 2 letter
---
+++ SimpleAddCalculator.py
@@ -0,0 +1,11 @@
+print("Enter Your First Number: ")
+
+num1 = int(input())
+
+print("Enter Your Second Number: ")
+
+num2 = int(input())
+
+sum = num1 + num2
+
+print("The Addition Is:",sum)
