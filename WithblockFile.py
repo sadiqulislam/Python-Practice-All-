b46050838ef73d0bbd041e4ed0ffbaf25afd6b4e
Shishir <sishishir2015@gmail.com>
2019-12-08 01:39:08 +0600

    New Testing

---
+++ WithblockFile.py
@@ -0,0 +1,3 @@
+with open('Shishir2.txt') as file:
+    content = file.read(10)
+    print(content)
---
+++ globalvariableKeyword.py
@@ -0,0 +1,9 @@
+g = 10
+
+def rise(m):
+    l = 10
+    b = g + l
+    print(b,m)
+
+
+rise("We")
