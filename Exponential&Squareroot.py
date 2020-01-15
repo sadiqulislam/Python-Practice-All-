9cb401e4ec44e87f7af0e463c10c5b01eba5681a
Shishir <sishishir2015@gmail.com>
2020-01-15 22:39:08 +0600

    Exponential; And Square Root

--- Exponential&Squareroot.py
+++ Exponential&Squareroot.py
@@ -1,2 +1,14 @@
 print(8**2)
 print(64**(1/2))
+
+a = int(input("Enter Your Number:"))
+b = int(input("Enter The Power:"))
+
+exp = a**b
+
+print(f"Exponential Of {a} is =",exp)
+
+c = float(input("Enter Your Number:"))
+sqrtt = (c**(1/2))
+
+print(f"Square Root Of {a} Is=",sqrtt)
