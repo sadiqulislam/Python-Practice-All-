7e8a55e06164a785d020f585eb283f650fd3e954
Shishir <sishishir2015@gmail.com>
2020-01-16 21:14:34 +0600

    For Loop And Function

--- Forloop.py
+++ Forloop.py
@@ -1,5 +1,13 @@
 sum = 0
 for sum in range(1,11):
     print(sum)
-    sum+=1
 
+fruits = ["Apple","Mango","Grapes","Pineapple"]
+
+for x in fruits:
+    print(x)
+
+name= ["Shishir","Saad","Ratul","Fahad","Farhan"]
+
+for item in name:
+    print(item)
---
+++ pms/Reuse_Function.py
@@ -0,0 +1,21 @@
+def Welcome():
+
+    x ="WelCome To Python"
+
+    print(x)
+
+Welcome()
+
+
+def Name(x):
+
+    print(f"Welcome {x}")
+
+Name("Shishir")
+
+def Info(name,age,country):
+
+    print(name,age,country)
+
+Info("Shishir","24","Bangladesh")
+Info("Shishir","24","Bangladesh")
