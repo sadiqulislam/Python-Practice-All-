285da4792fee2a6adc7f77edb03a4b6527462b97
Shishir <sishishir2015@gmail.com>
2020-02-10 19:33:37 +0600

    Regular Expression

--- Regular Expression/RegularExpression.py
+++ Regular Expression/RegularExpression.py
@@ -2,8 +2,25 @@ import re
 
 pattern= r"sishishir2015@gmail.com"
 
-if re.match(pattern,"sishishir2015@gmail.com"):
+if re.match(pattern,"@gmail.com"):
     print("Match Found")
 
 else:
     print(None)
+
+pattern2 = r"egg"
+
+if re.search(pattern2,"eggbaconeggbaconegg"):
+    print("Match Found")
+
+else:
+    print("Not Match")
+
+if re.findall(pattern2,"eggeggbaconeggbacon"):
+    print("Match Found")
+
+else:
+    print("Not Found")
+
+
+print(re.findall(pattern2,"eggbaconeggbaconeggegg"))
